0759bcb597b504670244b92029f7c32675581df0
Sean Ryan <sean@designingsean.com>
2013-01-26 20:29:16 -0500

    Rebuilt using angular. Split pay period in to two weeks.

--- api/index.php
+++ api/index.php
@@ -2,10 +2,15 @@
 date_default_timezone_set('America/New_York');
 require_once 'meekrodb.2.1.class.php';
 require_once 'db.config.php';
+DB::$param_char = '%%';
 
 $action = $_GET['action'];
 if (isset($_GET['user'])) {
 	$user = $_GET['user'];
+}
+
+function getTimes($user, $start, $end) {
+	return DB::query('SELECT DATE_FORMAT(clockIn, "%a, %b %e") as clockInDate, DATE_FORMAT(clockIn, "%l:%i %p") as clockInTime, DATE_FORMAT(clockOut, "%l:%i %p") as clockOutTime, ROUND(TIMESTAMPDIFF(MINUTE, clockIn, clockOut)/60, 2) as totalTime FROM clock WHERE uid=%%s AND (clockIn >= %%s AND clockIn < %%s) ORDER BY clockIn DESC', $user, $start, $end);
 }
 
 switch ($action) {
@@ -14,37 +19,50 @@ switch ($action) {
 		echo json_encode($results);
 		break;
 	case 'getStatus' :
-		$results = DB::queryFirstRow('SELECT clockIn, clockOut FROM clock WHERE uid=%s ORDER BY clockIn DESC', $user);
+		$results = DB::queryFirstRow('SELECT clockIn, clockOut FROM clock WHERE uid=%%s ORDER BY clockIn DESC', $user);
 		echo json_encode($results);
 		break;
 	case 'getCurrent' :
 		if ((date('W') % 2) == 0) {
 			if (date('w') == 1) {
-				$startDate = date('y-m-d', strtotime('today'));
+				$startDateW1 = date('y-m-d', strtotime('today'));
 			} else {
-				$startDate = date('y-m-d', strtotime('last Monday'));
+				$startDateW1 = date('y-m-d', strtotime('last Monday'));
 			}
-			$endDate = date('y-m-d', strtotime('next Monday +1 week'));
+			$endDateW1 = date('y-m-d', strtotime('next Monday'));
+			$startDateW2 = date('y-m-d', strtotime('next Monday'));
+			$endDateW2 = date('y-m-d', strtotime('next Monday +1 week'));
 		} else {
-			$startDate = date('y-m-d', strtotime('last Monday -1 week'));
-			$endDate = date('y-m-d', strtotime('next Monday'));
+			$startDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
+			if (date('w') == 1) {
+				$endDateW1 = date('y-m-d', strtotime('today'));
+				$startDateW2 = date('y-m-d', strtotime('today'));
+			} else {
+				$endDateW1 = date('y-m-d', strtotime('last Monday'));
+				$startDateW2 = date('y-m-d', strtotime('last Monday'));
+			}
+			$endDateW2 = date('y-m-d', strtotime('next Monday'));
 		}
-		$results = DB::query('SELECT clockIn, clockOut FROM clock WHERE uid=%s AND (clockIn >= %s AND clockIn < %s) ORDER BY clockIn DESC', $user, $startDate, $endDate);
+		$results = array(getTimes($user, $startDateW2, $endDateW2), getTimes($user, $startDateW1, $endDateW1));
 		echo json_encode($results);
 		break;
 	case 'getPrevious' :
 		if ((date('W') % 2) == 0) {
-			$startDate = date('y-m-d', strtotime('last Monday -2 week'));
+			$startDateW1 = date('y-m-d', strtotime('last Monday -2 week'));
+			$endDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
+			$startDateW2 = date('y-m-d', strtotime('last Monday -1 week'));
 			if (date('w') == 1) {
-				$endDate = date('y-m-d', strtotime('today'));
+				$endDateW2 = date('y-m-d', strtotime('today'));
 			} else {
-				$endDate = date('y-m-d', strtotime('last Monday'));
+				$endDateW2 = date('y-m-d', strtotime('last Monday'));
 			}
 		} else {
-			$startDate = date('y-m-d', strtotime('last Monday -3 week'));
-			$endDate = date('y-m-d', strtotime('last Monday -1 week'));
+			$startDateW1 = date('y-m-d', strtotime('last Monday -3 week'));
+			$endDateW1 = date('y-m-d', strtotime('last Monday -2 week'));
+			$startDateW2 = date('y-m-d', strtotime('last Monday -2 week'));
+			$endDateW2 = date('y-m-d', strtotime('last Monday -1 week'));
 		}
-		$results = DB::query('SELECT clockIn, clockOut FROM clock WHERE uid=%s AND (clockIn >= %s AND clockIn < %s) ORDER BY clockIn DESC', $user, $startDate, $endDate);
+		$results = array(getTimes($user, $startDateW2, $endDateW2), getTimes($user, $startDateW1, $endDateW1));
 		echo json_encode($results);
 		break;
 	case 'clockIn' :
@@ -54,10 +72,10 @@ switch ($action) {
 		));
 		break;
 	case 'clockOut' :
-		$results = DB::queryFirstRow('SELECT id FROM clock WHERE uid=%s ORDER BY id DESC', $user);
+		$results = DB::queryFirstRow('SELECT id FROM clock WHERE uid=%%s ORDER BY id DESC', $user);
 		DB::update('clock', array(
 			'clockOut' => date('Y-m-d H:i:s'),
-		), 'id=%s', $results['id']);
+		), 'id=%%s', $results['id']);
 		break;
 }
 ?>
---
+++ js/app.js
@@ -0,0 +1,91 @@
+function timeClock($scope, $http) {
+	$scope.currentUser = $scope.totalCurrent = $scope.totalPrevious = 0;
+	$scope.clockedIn = false;
+	
+	//get the list of users
+	$http.get("api/?action=getUsers").success(function(response) {
+		$scope.users = response;
+	});
+
+	//get a users current status
+	function currentStatus() {
+		$http.get("api/?action=getStatus&user=" + $scope.currentUser).success(function(response) {
+			var currentStatus;
+			var time;
+			if (response === null || (response.clockIn !== null && response.clockOut !== null) ) {
+				currentStatus = "Clocked out ";
+				time = response.clockOut;
+				$scope.clockedIn = false;
+			} else {
+				currentStatus = "Clocked in ";
+				time = response.clockIn;
+				$scope.clockedIn = true;
+			}
+			currentStatus = currentStatus + Date.create(time).relative(function(value, unit, ms, loc) {
+				if (ms.abs() > (14).hour() && ms.abs() < (7).day()) {
+					return "on {Weekday} at {12hr}:{mm}{tt}";
+				} else if (ms.abs() >= (7).day()) {
+					return "on {Weekday}, {Month} {d} at {12hr}:{mm}{tt}";
+				}
+			});
+			$scope.currentStatus = currentStatus
+		});
+	}
+
+	//get the current paycheck's hours
+	function getCurrent() {
+		$http.get("api/?action=getCurrent&user=" + $scope.currentUser).success(function(response) {
+			response[0].weekTotal = getTotal(response[0]);
+			response[1].weekTotal = getTotal(response[1]);
+			$scope.currentTimes = response;
+			$scope.totalCurrent = getTotal(response[0]) + getTotal(response[1]);
+		});
+	}
+
+	//get the previous paycheck's hours
+	function getPrevious() {
+		$http.get("api/?action=getPrevious&user=" + $scope.currentUser).success(function(response) {
+			response[0].weekTotal = getTotal(response[0]);
+			response[1].weekTotal = getTotal(response[1]);
+			$scope.previousTimes = response;
+			$scope.totalPrevious = getTotal(response[0]) + getTotal(response[1]);
+		});
+	}
+
+	//gets total time for a paycheck
+	function getTotal(obj) {
+		var total = 0;
+		angular.forEach(obj, function(value, key) {
+			if (obj.totalTime !== null)
+				total += (value.totalTime).toNumber();
+		});
+		return total.round(2);
+	}
+
+	//resets back to basics
+	function reset() {
+		$scope.currentTimes = {};
+		$scope.previousTimes = {};
+		$scope.clockedIn = false;
+		$scope.currentStatus = "";
+		$scope.currentUser = $scope.totalCurrent = $scope.totalPrevious = 0;
+	}
+
+	$scope.getTimes = function() {
+		if($scope.currentUser > 0) {
+			currentStatus();
+			getCurrent();
+			getPrevious();
+		} else {
+			reset();
+		}
+	};
+
+	$scope.clock = function(inOut) {
+		$http.get("api/", { params: { action: inOut, user: $scope.currentUser } }).success($scope.getTimes).error(function() { alert("error"); });
+	}
+
+	$scope.reset = function() {
+		reset();
+	}
+}
