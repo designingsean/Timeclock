3d73939521a81171d4b4c72cf9c7b251b80dcb8e
Sean Ryan <sean@designingsean.com>
2013-06-01 22:50:32 -0400

    Complete rewrite of API.

--- api/index.php
+++ api/index.php
@@ -4,117 +4,85 @@ require_once 'meekrodb.2.1.class.php';
 require_once 'db.config.php';
 DB::$param_char = '%%';
 
-$action = $_GET['action'];
-if (isset($_GET['user'])) {
-	$user = $_GET['user'];
+switch ($_GET['action']) {
+
+    //users
+    case 'usersAdd' :
+        //-add = add new employee
+        //---pass in name (required)
+        //---return success/fail
+        if (isset($_GET['name'])) {
+            $results = DB::insert('users', array('name' => $_GET['name']));
+        }
+    break;
+
+    case 'usersGet' :
+        //-get = employee drop down, list of active/inactive users
+        //---pass in active (required)
+        //---return id, name, active
+        if (isset($_GET['active'])) {
+            $results = DB::query('SELECT id, name, active FROM users WHERE active=%%s ORDER BY name ASC', $_GET['active']);
+        }
+    break;
+
+    case 'usersUpdate' :
+        // update = change status
+        // pass in id (required)
+        // return success/fail
+        if (isset($_GET['id'])) {
+            $results = DB::update('users', array('active' => DB::sqleval("NOT active")), 'id=%%s', $_GET['id']);
+        }
+    break;
+
+    //clock
+    case 'clockAdd' :
+        //-add = clock in, add new
+        //---pass in user id (required), time in (optional with default), time out (optional)
+        //---return success/fail
+        if (isset($_GET['user'])) {
+            $columns = array();
+            $columns['uid'] = $_GET['user'];
+            $columns['clockIn'] = isset($_GET['start']) ? $_GET['start'] : date('Y-m-d H:i:s');
+            $columns['clockOut'] = isset($_GET['end']) ? $_GET['end'] : NULL;
+            $results = DB::insert('clock', $columns);
+        }
+    break;
+
+    case 'clockGet' :
+        //-get = current, past, report
+        //---pass in start date (required), end date (required), user id (required)
+        //---return id, user id, date, clock in, clock out
+        if (isset($_GET['user']) && isset($_GET['start']) && isset($_GET['end'])) {
+            $results = DB::query('SELECT id, DATE_FORMAT(clockIn, "%a, %b %e") as clockInDate, DATE_FORMAT(clockIn, "%l:%i %p") as clockInTime, DATE_FORMAT(clockOut, "%l:%i %p") as clockOutTime, ROUND(TIMESTAMPDIFF(MINUTE, clockIn, clockOut)/60, 2) as totalTime FROM clock WHERE uid=%%s AND (clockIn >= %%s AND clockIn < %%s) ORDER BY clockIn DESC', $_GET['user'], $_GET['start'], $_GET['end']);
+        }
+    break;
+
+    case 'clockUpdate' :
+        //-update = clock out, edit
+        //---pass in id (required), time in (optional), time out (optional)
+        //---return success/fail
+        if (isset($_GET['id'])) {
+            $columns = array();
+            if (isset($_GET['start'])) {
+                $columns['clockIn'] = $_GET['start'];
+            }
+            if (isset($_GET['end'])) {
+                $columns['clockOut'] = $_GET['end'];
+            }
+            $results = DB::update('clock', $columns, "id=%%s", $_GET['id']);
+        }
+    break;
+
+    case 'clockDelete' :
+        //-delete = remove error
+        //---pass in id (required)
+        //---return success/fail
+        if (isset($_GET['id'])) {
+            $results = DB::delete('clock', "id=%%s", $_GET['id']);
+        }
+    break;
+
 }
 
-function getTimes($user, $start, $end) {
-	return DB::query('SELECT id, DATE_FORMAT(clockIn, "%a, %b %e") as clockInDate, DATE_FORMAT(clockIn, "%l:%i %p") as clockInTime, DATE_FORMAT(clockOut, "%l:%i %p") as clockOutTime, ROUND(TIMESTAMPDIFF(MINUTE, clockIn, clockOut)/60, 2) as totalTime FROM clock WHERE uid=%%s AND (clockIn >= %%s AND clockIn < %%s) ORDER BY clockIn DESC', $user, $start, $end);
-}
-
-switch ($action) {
-	//base functions
-	case 'getUsers' :
-		$results = DB::query('SELECT id, name FROM users WHERE active=1 ORDER BY name ASC');
-		echo json_encode($results);
-		break;
-	case 'getStatus' :
-		$results = DB::queryFirstRow('SELECT clockIn, clockOut FROM clock WHERE uid=%%s ORDER BY clockIn DESC', $user);
-		echo json_encode($results);
-		break;
-	case 'getCurrent' :
-		if ((date('W') % 2) == 0) {
-			if (date('w') == 1) {
-				$startDateW1 = date('y-m-d', strtotime('today'));
-			} else {
-				$startDateW1 = date('y-m-d', strtotime('last Monday'));
-			}
-			$endDateW1 = date('y-m-d', strtotime('next Monday'));
-			$startDateW2 = date('y-m-d', strtotime('next Monday'));
-			$endDateW2 = date('y-m-d', strtotime('next Monday +1 week'));
-		} else {
-			if (date('w') == 1) {
-				$startDateW1 = date('y-m-d', strtotime('last Monday'));
-				$endDateW1 = date('y-m-d', strtotime('today'));
-				$startDateW2 = date('y-m-d', strtotime('today'));
-			} else {
-				$startDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
-				$endDateW1 = date('y-m-d', strtotime('last Monday'));
-				$startDateW2 = date('y-m-d', strtotime('last Monday'));
-			}
-			$endDateW2 = date('y-m-d', strtotime('next Monday'));
-		}
-		$results = array(getTimes($user, $startDateW2, $endDateW2), getTimes($user, $startDateW1, $endDateW1));
-		echo json_encode($results);
-		break;
-	case 'getPrevious' :
-		if ((date('W') % 2) == 0) {
-			if (date('w') == 1) {
-				$startDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
-				$endDateW1 = date('y-m-d', strtotime('last Monday'));
-				$startDateW2 = date('y-m-d', strtotime('last Monday'));
-				$endDateW2 = date('y-m-d', strtotime('today'));
-			} else {
-				$startDateW1 = date('y-m-d', strtotime('last Monday -2 week'));
-				$endDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
-				$startDateW2 = date('y-m-d', strtotime('last Monday -1 week'));
-				$endDateW2 = date('y-m-d', strtotime('last Monday'));
-			}
-		} else {
-			if (date('w') == 1) {
-				$startDateW1 = date('y-m-d', strtotime('last Monday -2 week'));
-				$endDateW1 = date('y-m-d', strtotime('last Monday -1 week'));
-				$startDateW2 = date('y-m-d', strtotime('last Monday -1 week'));
-				$endDateW2 = date('y-m-d', strtotime('last Monday'));
-			} else {
-				$startDateW1 = date('y-m-d', strtotime('last Monday -3 week'));
-				$endDateW1 = date('y-m-d', strtotime('last Monday -2 week'));
-				$startDateW2 = date('y-m-d', strtotime('last Monday -2 week'));
-				$endDateW2 = date('y-m-d', strtotime('last Monday -1 week'));
-			}
-		}
-		$results = array(getTimes($user, $startDateW2, $endDateW2), getTimes($user, $startDateW1, $endDateW1));
-		echo json_encode($results);
-		break;
-	case 'clockIn' :
-		DB::insert('clock', array(
-			'uid' => $user,
-			'clockIn' => date('Y-m-d H:i:s')
-		));
-		break;
-	case 'clockOut' :
-		$results = DB::queryFirstRow('SELECT id FROM clock WHERE uid=%%s ORDER BY id DESC', $user);
-		DB::update('clock', array(
-			'clockOut' => date('Y-m-d H:i:s'),
-		), 'id=%%s', $results['id']);
-		break;
-
-	//administrative functions
-	case 'adminAdd' :
-		DB::insert('clock', array(
-			'uid' => $user,
-			'clockIn' => $_GET['start'],
-			'clockOut' => $_GET['end'],
-		));
-		break;
-	case 'adminDelete' :
-		DB::delete('clock', "id=%%s", $_GET['id']);
-		break;
-	case 'adminAddUser' :
-		DB::insert('users', array(
-			'name' => $_GET['user']
-		));
-		break;
-	case 'adminUpdateUser' :
-		DB::update('users', array(
-			'active' => $_GET['active'],
-		), 'id=%%s', $_GET['id']);
-		break;
-	case 'adminInactiveUsers' :
-		$results = DB::query('SELECT id, name FROM users WHERE active=0 ORDER BY name ASC');
-		echo json_encode($results);
-		break;
-	//reporting functions
-}
-?>
+//return query results as JSON
+echo json_encode($results);
