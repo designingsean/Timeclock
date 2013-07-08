e18e3cbb0aa76cc8f5ff98fd8b0a70d2ca70195a
Sean Ryan <sean@designingsean.com>
2013-07-08 00:44:00 -0400

    Added HTML base tag and updated app routes accordingly.

--- app/app.js
+++ app/app.js
@@ -2,10 +2,10 @@ var timeclock = angular.module("timeclock", [])
 .config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        .when("/timeclock", { controller: "clock", templateUrl: "/timeclock/app/views/clock.html" })
-        .when("/timeclock/admin/", { controller: "edit", templateUrl: "/timeclock/app/views/edit.html" })
-        .when("/timeclock/admin/report", { controller: "report", templateUrl: "/timeclock/app/views/report.html" })
-        .when("/timeclock/admin/users", { controller: "users", templateUrl: "/timeclock/app/views/users.html" })
+        .when("/", { controller: "clock", templateUrl: "app/views/clock.html" })
+        .when("/admin/", { controller: "edit", templateUrl: "app/views/edit.html" })
+        .when("/admin/report", { controller: "report", templateUrl: "app/views/report.html" })
+        .when("/admin/users", { controller: "users", templateUrl: "app/views/users.html" })
         .otherwise({ redirectTo: '/timeclock' });
 });
 timeclock.factory('usersApi', ['$http', function($http) {
